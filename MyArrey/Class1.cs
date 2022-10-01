edf715873044236af561eda07886bcb3d549e36f
HP <HP@DESKTOP-EQ9MNTC>
2022-10-02 01:33:38 +0400

    Добавьте файлы проекта.

---
+++ MyArrey/Class1.cs
@@ -0,0 +1,12 @@
+﻿using Homework3;
+
+int size = 11;
+
+var array = new MyArray(size);
+Console.WriteLine(array);
+
+array.Sort(size);
+Console.WriteLine(array);
+
+array.Reverse(size);
+Console.WriteLine(array);
---
+++ MyArrey/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3;
+
+class MyArray
+{
+    public int[] array;
+
+    public MyArray(int size)
+    {
+        array = new int[size];
+
+        var random = new Random();
+
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = random.Next(0, 10);
+        }
+    }
+
+    public void Sort(int size)
+    {
+        // Bubble sort algorithm here
+
+        int temp;
+        array = new int[size];
+
+
+        for (int i = 0; i < size; i++)
+        {
+            if (array[0] > array[i])
+            {
+                temp = array[0];
+                array[0] = array[i];
+                array[i] = temp;
+            }
+
+        }
+    }
+
+    public void Reverse(int size)
+    {
+        // Reverse array algorithm here
+
+        array = new int[size];
+
+
+        for (int i = 0; i < array.Length / 2; i++)
+        {
+            int temp = array[i];
+            array[i] = array[array.Length - i - 1];
+            array[array.Length - i - 1] = temp;
+        }
+    }
+
+    public override string ToString()
+    {
+        // Output using StringBuilder
+        var builder = new StringBuilder();
+        int size = 11;
+
+        array = new int[size];
+
+        var random = new Random();
+
+        for (int i = 0; i < size; i++)
+        {
+            builder.Append($"{array[i]} ");
+        }
+        builder.Append('\n');
+
+        return builder.ToString();
+
+
+    }
+
+}
